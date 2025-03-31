ea5aa3ab765b8c074401827511f1074f9344331c
Flapjack12 <admiraljackbar07@gmail.com>
2025-03-31 11:45:23 +0100

    hdsgikRSUGwjg8gyuehigheuiyhgjesbgu

--- src/MealPlan.java
+++ src/MealPlan.java
@@ -1,4 +1,17 @@
-public class MealPlan {
-    HashMap<String day, Recipe> weeklyMealPlan;
+import java.util.HashMap;
 
+public class MealPlan {
+    private HashMap<String, Recipe> weeklyMealPlan = new HashMap<>();
+
+    public void addMeal(String day, Recipe recipe) {
+        weeklyMealPlan.put(day, recipe);
+    }
+
+    public Recipe getMealForDay(String day) {
+        return weeklyMealPlan.getOrDefault(day, null);
+    }
+
+    public ShoppingList generateShoppingList(HashMap weeklyMealPlan) {
+        //generate shopping list
+    }
 }
