9c5279e8ea0efa4dc0f3b70e628b745fb4d5f6af
Flapjack12 <admiraljackbar07@gmail.com>
2025-05-13 12:36:31 +0100

    binary searching for strings yippie

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,8 +1,10 @@
 public class BinarySearchTree {
     private BinaryTreeNode root;
+    private BinaryTreeNodeString stringRoot;
 
     public BinarySearchTree() {
         root = null;
+        stringRoot = null;
     }
     //insert a new node preserving the BST property
     public void add(int v) {
@@ -19,6 +21,39 @@ public class BinarySearchTree {
         boolean done = false;
         while (!done) {
             if (v <= current.getValue()) {
+                if (current.getLeft() == null) {
+                    //we've arrived
+                    current.setLeft(leaf);
+                    done = true;
+                } else {
+                    current = current.getLeft();
+                }
+            } else {
+                if (current.getRight() == null) {
+                    //we've arrived
+                    current.setRight(leaf);
+                    done = true;
+                } else {
+                    current = current.getRight();
+                }
+            }
+        }
+    }
+
+    public void add(String v) {
+        BinaryTreeNodeString leaf = new BinaryTreeNodeString(v);
+
+        //trivial case tree is empty
+        if (stringRoot == null) {
+            stringRoot = leaf;
+            return;
+        }
+
+        //for all others find the correct point of insertion
+        BinaryTreeNodeString current = stringRoot;
+        boolean done = false;
+        while (!done) {
+            if (v.compareToIgnoreCase(current.getValue()) == 0 || v.compareToIgnoreCase(current.getValue()) == -1) { //less than or equal to
                 if (current.getLeft() == null) {
                     //we've arrived
                     current.setLeft(leaf);
--- src/Main.java
+++ src/Main.java
@@ -12,46 +12,56 @@ public class Main {
 
          */
 
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter the name of the recipe:");
+//        String name = scanner.nextLine();
+//
+//        System.out.println("Enter the book name:");
+//        String book = scanner.nextLine();
+//
+//        System.out.println("Enter the page number:");
+//        int page = Integer.parseInt(scanner.nextLine());
+//
+//        System.out.println("Enter ingredients separated by commas:");
+//        Ingredient[] ingredients = Arrays.stream(scanner.nextLine().split(","))
+//                .map(String::trim)
+//                .map(Ingredient::new)
+//                .toArray(Ingredient[]::new);
+//
+//        System.out.println("Enter calories:");
+//        int calories = Integer.parseInt(scanner.nextLine());
+//
+//        System.out.println("Enter protein content:");
+//        int protein = Integer.parseInt(scanner.nextLine());
+//
+//        System.out.println("Enter carbs content:");
+//        int carbs = Integer.parseInt(scanner.nextLine());
+//
+//        System.out.println("Enter sugar content:");
+//        int sugars = Integer.parseInt(scanner.nextLine());
+//
+//        System.out.println("Enter fat content:");
+//        int fats = Integer.parseInt(scanner.nextLine());
+//
+//        System.out.println("Enter tags separated by commas:");
+//        String[] tags = scanner.nextLine().split(",");
+//
+//        //Recipe newRecipe = new Recipe(name, book, page, ingredients, calories, protein, carbs, sugars, fats, tags);
+//        FileHandler.addRecipe(name, book, page, ingredients, calories, protein, carbs, sugars, fats, tags);
+//
+//        System.out.println("New recipe added successfully!");
+//
+//        System.out.println(FileHandler.recipeFileRead());
 
-        System.out.println("Enter the name of the recipe:");
-        String name = scanner.nextLine();
+        String[] data = {"house","car","wall","dog","apple","soup","School","Alphabetical","Zoo"};
 
-        System.out.println("Enter the book name:");
-        String book = scanner.nextLine();
+        BinarySearchTree bst = new BinarySearchTree();
+        for (int i = 0; i < data.length; i++) {
+            System.out.println("adding " + data[i]);
+            bst.add(data[i]);
+        }
 
-        System.out.println("Enter the page number:");
-        int page = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Enter ingredients separated by commas:");
-        Ingredient[] ingredients = Arrays.stream(scanner.nextLine().split(","))
-                .map(String::trim)
-                .map(Ingredient::new)
-                .toArray(Ingredient[]::new);
-
-        System.out.println("Enter calories:");
-        int calories = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Enter protein content:");
-        int protein = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Enter carbs content:");
-        int carbs = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Enter sugar content:");
-        int sugars = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Enter fat content:");
-        int fats = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Enter tags separated by commas:");
-        String[] tags = scanner.nextLine().split(",");
-
-        //Recipe newRecipe = new Recipe(name, book, page, ingredients, calories, protein, carbs, sugars, fats, tags);
-        FileHandler.addRecipe(name, book, page, ingredients, calories, protein, carbs, sugars, fats, tags);
-
-        System.out.println("New recipe added successfully!");
-
-        System.out.println(FileHandler.recipeFileRead());
+        bst.display();
     }
 }
