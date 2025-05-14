8e5098ecaa5467956b45a0a8790c68b02fa36343
Flapjack12 <admiraljackbar07@gmail.com>
2025-05-14 12:35:42 +0100

    GUI part two bazinga

--- src/GUI.java
+++ src/GUI.java
@@ -1,16 +1,56 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI extends JFrame {
     JLabel lblMessage;
+    JButton button;
 
     public GUI() {
         setTitle("New Window");
+        setSize(600,400);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
         lblMessage = new JLabel("New Label");
-        lblMessage.setBounds(50, 50, 150, 30);
+        lblMessage.setBounds(50, 50, 150, 150);
+        lblMessage.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+
+        button = new JButton("bazinga");
+        button.setBounds(75,75,150,30);
+
+        button = new JButton("bazinga");
+        button.setBounds(75,75,150,30);
+
+        button = new JButton("bazinga");
+        button.setBounds(75,75,150,30);
+
+        button = new JButton("bazinga");
+        button.setBounds(75,75,150,30);
+
+        button = new JButton("bazinga");
+        button.setBounds(75,75,150,30);
+
+        button = new JButton("bazinga");
+        button.setBounds(75,75,150,30);
+
+        button = new JButton("bazinga");
+        button.setBounds(75,75,150,30);
+
+        button = new JButton("bazinga");
+        button.setBounds(75,75,150,30);
+
+        button = new JButton("bazinga");
+        button.setBounds(75,75,150,30);
+
+        button = new JButton("bazinga");
+        button.setBounds(75,75,150,30);
+
+        button = new JButton("bazinga");
+        button.setBounds(75,75,150,30);
+
+        add(lblMessage);
+
     }
 }
