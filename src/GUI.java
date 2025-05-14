155a4e6e0fb9e3ae0fe2a70c512ab321df95b692
atjack19 <admiraljackbar07@gmail.com>
2025-05-14 19:30:12 +0100

    continuing GUI - jpanels maybe?

--- src/GUI.java
+++ src/GUI.java
@@ -3,7 +3,20 @@ import java.awt.*;
 
 public class GUI extends JFrame {
     JLabel lblMessage;
-    JButton button;
+    JButton recipeButton;
+    JButton stockButton;
+    JButton monButton;
+    JButton tueButton;
+    JButton wedButton;
+    JButton thuButton;
+    JButton friButton;
+    JButton satButton;
+    JButton sunButton;
+    JButton listButton;
+    JButton clearButton;
+    int standardWidth = 150;
+    int standardHeight = 30;
+    int containerWidth = getContentPane().getWidth();
 
     public GUI() {
         setTitle("New Window");
@@ -14,43 +27,53 @@ public class GUI extends JFrame {
         setVisible(true);
 
         lblMessage = new JLabel("New Label");
-        lblMessage.setBounds(50, 50, 150, 150);
+        lblMessage.setBounds(50, 50, standardWidth, 150);
         lblMessage.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
 
-        button = new JButton("bazinga");
-        button.setBounds(75,75,150,30);
+        recipeButton = new JButton("Recipes");
+        recipeButton.setBounds(75,50,standardWidth,standardHeight);
 
-        button = new JButton("bazinga");
-        button.setBounds(75,75,150,30);
+        stockButton = new JButton("Stock");
+        stockButton.setBounds(-50,50,standardWidth,standardHeight);
 
-        button = new JButton("bazinga");
-        button.setBounds(75,75,150,30);
+        monButton = new JButton("Monday");
+        monButton.setBounds(75,75,150,30);
 
-        button = new JButton("bazinga");
-        button.setBounds(75,75,150,30);
+        tueButton = new JButton("Tuesday");
+        tueButton.setBounds(75,75,150,30);
 
-        button = new JButton("bazinga");
-        button.setBounds(75,75,150,30);
+        wedButton = new JButton("Wednesday");
+        wedButton.setBounds(75,75,150,30);
 
-        button = new JButton("bazinga");
-        button.setBounds(75,75,150,30);
+        thuButton = new JButton("Thursday");
+        thuButton.setBounds(75,75,150,30);
 
-        button = new JButton("bazinga");
-        button.setBounds(75,75,150,30);
+        friButton = new JButton("Friday");
+        friButton.setBounds(75,75,150,30);
 
-        button = new JButton("bazinga");
-        button.setBounds(75,75,150,30);
+        satButton = new JButton("Saturday");
+        satButton.setBounds(75,75,150,30);
 
-        button = new JButton("bazinga");
-        button.setBounds(75,75,150,30);
+        sunButton = new JButton("Sunday");
+        sunButton.setBounds(75,75,150,30);
 
-        button = new JButton("bazinga");
-        button.setBounds(75,75,150,30);
+        listButton = new JButton("List");
+        listButton.setBounds(75,75,150,30);
 
-        button = new JButton("bazinga");
-        button.setBounds(75,75,150,30);
+        clearButton = new JButton("Clear");
+        clearButton.setBounds(75,75,150,30);
 
-        add(lblMessage);
+        add(recipeButton);
+        add(stockButton);
+        //add(monButton);
+        //add(tueButton);
+        //add(wedButton);
+        //add(thuButton);
+        //add(friButton);
+        //add(satButton);
+        //add(sunButton);
+        //add(listButton);
+        //add(clearButton);
 
     }
 }
